73a7a2ffccc22d284e7a6953e0ee51beb4d8688c
Alex Stockwell <astockwell@gmail.com>
2015-03-29 22:45:19 -0700

    added automatic updater framework

---
+++ updaters/lib/locale.rb
@@ -0,0 +1,18 @@
+###
+# Represents a single province/state/region
+#
+class Locale < Struct.new(:name, :code, :native)
+	def to_map
+		map = Hash.new
+		self.members.each { |m| map[m] = self[m] unless self[m].nil? }
+		map
+	end
+
+	def to_json(*a)
+		to_map.to_json(*a)
+	end
+
+	def <=>(other)
+		self[:name] <=> other[:name]
+	end
+end
