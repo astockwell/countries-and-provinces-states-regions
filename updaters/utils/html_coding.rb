61a129a24622e944854d3f01e0993f08f1944f6c
Alex Stockwell <astockwell@gmail.com>
2015-03-29 22:44:42 -0700

    added venezuela (automated)

--- updaters/utils/html_coding.rb
+++ updaters/utils/html_coding.rb
@@ -1,13 +1,17 @@
 require 'json'
 require 'htmlentities'
 
+# USEAGE:
+# require '../utils/html_coding'
+# HtmlCoding.Decode(string)
+
 Encoding.default_internal = Encoding::UTF_8
 Encoding.default_external = Encoding::UTF_8
 
-@coder = HTMLEntities.new
+module HtmlCoding
+	@coder = HTMLEntities.new
 
-states = JSON.parse( File.read('../../countries/thailand.json') )
-
-states.each do |s|
-	p @coder.decode s['name']
+	def self.Decode(s)
+		@coder.decode(s)
+	end
 end
