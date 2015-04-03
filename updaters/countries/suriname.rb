88aa4e2a4e59656e6fd1058b6cfadecfb0182fc7
Alex Stockwell <astockwell@gmail.com>
2015-04-02 21:23:33 -0700

    remove South American updaters and roll up to continent updater

--- updaters/countries/suriname.rb
+++
@@ -1,5 +0,0 @@
-require '../lib/get_from_wikipedia'
-
-filename = File.basename(__FILE__, ".rb")
-
-GetFromWikipedia.Scrape("SR", filename)
--- updaters/countries/venezuela.rb
+++
@@ -1,7 +0,0 @@
-require '../lib/get_from_wikipedia'
-
-filename = File.basename(__FILE__, ".rb")
-
-GetFromWikipedia.Scrape("VE", filename) do |name|
-	name.gsub("Distrito Federal") { |match| "Distrito Capital" }
-end
