b17b01722c62e41013d22bfc6754d28707a0b6f5
Alex Stockwell <astockwell@gmail.com>
2015-04-24 22:10:44 -0700

    wholesale update to the wikipedia getter lib, added Oceania automation, cleaned up jsons

---
+++ updaters/continents/oceania.rb
@@ -0,0 +1,61 @@
+require 'json'
+require 'active_support'
+require 'active_support/core_ext/string'
+require '../lib/get_from_wikipedia'
+
+countries = JSON.parse(IO.read("../../countries.json").force_encoding("UTF-8").encode!)
+
+countries.select! { |c| c["continent"] == "Oceania" }
+
+def fetch_country(code, name)
+	case code
+	when "AS" # american-samoa
+		return
+	# when "AU" # australia
+	when "CX" # christmas-island
+		return
+	when "CC" # cocos-keeling-islands
+		return
+	# when "FJ" # fiji
+	when "PF" # french-polynesia
+		return
+	# when "KI" # kiribati
+	# when "MH" # marshall-islands
+	# when "FM" # micronesia-federated-states-of
+	# when "NR" # nauru
+	when "NC" # new-caledonia
+		return
+	when "NZ" # new-zealand
+		GetFromWikipedia.Scrape(code, name, tables: [
+			TableIndexMap.new(1, 0, "Island"),
+			TableIndexMap.new(1, 0, 3, 2),
+		])
+	when "NF" # norfolk-island
+		return
+	# when "PW" # palau
+	# when "PG" # papua-new-guinea
+	# when "WS" # samoa
+	# when "SB" # solomon-islands
+	# when "TL" # timor-leste
+	# when "TO" # tonga
+	# when "TV" # tuvalu
+	# when "VU" # vanuatu
+	else
+		GetFromWikipedia.Scrape(code, name)
+	end
+	p code + " - " + name
+end
+
+countries.each do |country|
+	n = country["name"].gsub(/\(.*?\)/){ |m| "" }.parameterize
+	c = country["code"]
+
+	fetch_country(c, n)
+end
+
+
+# tables = [
+# 	TableIndexMap.new(1, 0, "Island"),
+# 	TableIndexMap.new(1, 0, 3, 2),
+# ]
+# GetFromWikipedia.Scrape("NZ", "new-zealand", tables: tables)
--- updaters/lib/get_from_wikipedia.rb
+++ updaters/lib/get_from_wikipedia.rb
@@ -1,33 +1,61 @@
 require 'json'
 require 'open-uri'
 require 'nokogiri'
-require_relative 'locale'
+require_relative '../lib/locale'
+
+TableIndexMap = Locale # :name, :code, :subdivision, :native
 
 # TODO: Get a better source than wikipedia
 module GetFromWikipedia
-	def self.Scrape(code, filename, options={})
-		starting_xpath = options[:starting_xpath] || '//*[@id="mw-content-text"]/table[1]//tr'
-		nested_xpath = options[:nested_xpath] || 'td[position() >= 1 and not(position() > 2)]/*[not(contains(@class, "sortkey") or contains(@class, "flagicon") or contains(@class, "image"))]'
-		code_td_index = options[:code_td_index] || 0
-		name_td_index = options[:name_td_index] || 1
+	SOURCE_TMPL = "http://en.wikipedia.org/wiki/ISO_3166-2:%s"
+	TABLE_SEARCH_TMPL = "//table[contains(@class, 'wikitable')][%d]"
+	EXCLUDED_TAGS = [".sortkey", ".flagicon", ".image"]
 
-		@SOURCE = "http://en.wikipedia.org/wiki/ISO_3166-2:#{code}"
+	def self.Scrape(code, filename, options={}, &block)
+		@tables = options[:tables] || [TableIndexMap.new(1, 0)]
+
 		@results = []
 
-		doc = Nokogiri::HTML( open(@SOURCE) )
+		doc = Nokogiri::HTML( open(SOURCE_TMPL % code) )
 
-		doc.xpath(starting_xpath).each do |tr|
-			tds = tr.xpath(nested_xpath)
-			unless tds.size < 2
-				begin
-					name = preprocess(tds[name_td_index].text)
-					name = yield(name) if block_given?
-					code = tds[code_td_index].text
-					@results << Locale.new(name, code)
- 				rescue Exception => e
-					puts "Error encountered parsing tds (#{tds}) into Locale: #{e}"
+		@tables.each_with_index do |table_indices, index|
+			next unless table_indices
+
+			one_based_index = index + 1
+			table = doc.search( TABLE_SEARCH_TMPL % one_based_index )
+
+			rows = table.css('tr')
+
+			rows.shift # remove table header row
+
+			rows.each do |row|
+				tds = row.search('td')
+
+				EXCLUDED_TAGS.each do |tag|
+					tds.css(tag).remove
 				end
+
+				loc = Locale.new
+
+				loc.members.each do |prop|
+					if table_indices[prop]
+						if table_indices[prop].is_a? Integer
+							if tds[table_indices[prop]] and !tds[table_indices[prop]].content.empty?
+								loc[prop] = tds[table_indices[prop]].content
+							end
+						else
+							loc[prop] = table_indices[prop]
+						end
+					end
+
+					loc[prop] = preprocess(loc[prop]) if loc[prop]
+					loc[prop] = yield(loc[prop], prop) if loc[prop] and block_given?
+				end
+
+				# p loc
+				@results << loc
 			end
+
 		end
 
 		File.open( "../../countries/#{filename}.json", 'w' ) do |writer|
@@ -37,6 +65,8 @@ module GetFromWikipedia
 
 	def self.preprocess(s)
 		s = s.gsub(" ") { |match| "" } # remove magical 0x2022 character
+		s = s.gsub(/\[.*?\]/) { |match| "" } # remove braces
+		s = s.gsub(/\(.*?\)/) { |match| "" } # remove parentheses
 		return s.strip
 	end
 end
